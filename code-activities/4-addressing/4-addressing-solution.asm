495db40a48e9f4204e80f28e7ed35f840d0b69d9
Victor Bushong <victorbush@gmail.com>
2024-01-02 20:30:04 -0600

    Add addressing code activity.

---
+++ code-activities/4-addressing/4-addressing-solution.asm
@@ -0,0 +1,30 @@
+extrn ExitProcess : proc        ; Declare external function ExitProcess
+
+.DATA                           ; Directive; Enter .data section
+
+testArray QWORD 20h, 21h, 22h, 23h, 24h, 25h
+
+.CODE                           ; Directive: Enter .code section
+
+main PROC                       ; Directive: Begin function labeled `main`
+
+    sub rsp, 28h                ; Bump 8 bytes to ensure 16 byte alignment. Reserve 32 bytes shadow space.
+    ; -------------------- /\ PROLOGUE /\ --------------------
+
+    ; Activity 1
+
+    mov rax, OFFSET [testArray + 8]     ; Gets address of second element in the array and stores it in RAX (remember that each element is 8 bytes each)
+    mov rbx, [rax]                      ; Gets the value in memory at the address stored in RAX and stores the value in RBX
+
+    ; Activity 2
+
+    mov rdx, [testArray]                ; Gets value of first element in the array
+    add rdx, [testArray + 5 * 8]        ; Multiplies RDX by the last element. Why use 5 * 8 here? It means (array index * size of each element).
+
+    ; -------------------- \/ EPILOGUE \/ --------------------
+    xor rcx, rcx                ; Clear RCX
+    call ExitProcess            ; Use Windows API to exit the process
+
+main ENDP                       ; Directive: End function labeled `main`
+
+END
---
+++ code-activities/4-addressing/README.md
@@ -0,0 +1,21 @@
+# Code Activity - Addressing
+
+Define the following variable in the data segment of the program:
+
+```
+testArray QWORD 20h, 21h, 22h, 23h, 24h, 25h
+```
+
+This array will be used for the following activities.
+
+_Note: The array contains QWORD elements, so each element is 8 bytes._
+
+1. Activity 1
+
+Use `MOV` and `OFFSET` to get the address of the second element in the array. Store the address in `RAX`.
+
+Now use a `MOV` instruction using `RAX` as a memory operand to get the value of the second element. Store the value in `RBX`.
+
+2. Activity 2
+
+Add the value of the first element and last element in the array and store the result in `RDX`.
