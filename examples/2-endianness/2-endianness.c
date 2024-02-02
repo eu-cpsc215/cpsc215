821b1ed7cb9cf24cb1d8039d1dc86f5556480210
Victor Bushong <victorbush@gmail.com>
2024-02-01 22:50:18 -0600

    Use stdint types for clarity in endianness example.

--- examples/2-endianness/2-endianness.c
+++ examples/2-endianness/2-endianness.c
@@ -3,17 +3,17 @@
 int main()
 {
 	uint32_t fourByteInt = 0x12345678;     // Store 4-byte integer value in memory
-	char* bytePtr = (char*)&fourByteInt;   // Get a pointer to that value in memory.
-	char first = bytePtr[0];
-	char second = bytePtr[1];
-	char third = bytePtr[2];
-	char fourth = bytePtr[3];
+	uint8_t* bytePtr = (uint8_t*)&fourByteInt;   // Get a pointer to that value in memory.
+	uint8_t first = bytePtr[0];
+	uint8_t second = bytePtr[1];
+	uint8_t third = bytePtr[2];
+	uint8_t fourth = bytePtr[3];
 
 	// Set breakpoint and observe the values of the variables above.
 	// Use memory debug window and observe value of bytes in memory.
 
 	uint16_t twoByteInt = 0x2143;
-	bytePtr = (char*)&twoByteInt;
+	bytePtr = (uint16_t*)&twoByteInt;
 	first = bytePtr[0];
 	second = bytePtr[1];
 
